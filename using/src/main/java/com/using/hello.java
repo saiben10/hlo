aadd01992d4ef31d4e7d11672651c09e8dcb201f
saiben10 <vallabhanenisai1999@gmail.com>
2021-09-11 17:08:07 +0530

    for

---
+++ using/src/main/java/com/using/hello.java
@@ -0,0 +1,11 @@
+package com.using;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java project using maven");
+
+	}
+
+}
